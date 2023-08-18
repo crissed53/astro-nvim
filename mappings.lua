768d9d64ae4bab6cc6eb293c5fef4b09d026cc2a
crissed <jisu.jeon53@navercorp.com>
2023-08-18 22:20:11 +0000

    update

--- mappings.lua
+++ mappings.lua
@@ -29,6 +29,7 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
+    ["<C-n>"] = { "<cmd> Neotree toggle <CR>" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
